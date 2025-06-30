e0549fd5ae29bba5853484a390be39b0f607e396
zaarouri <abdelmounime.zaarouri@gmail.com>
2025-06-30 13:18:54 +0100

    w1d5p2

--- week1/day5/mini-project/ticTacToe/ticTacToe.py
+++ week1/day5/mini-project/ticTacToe/ticTacToe.py
@@ -1,78 +1,65 @@
 class TicTacToe:
     def __init__(self):
-        self.board = [''] * 9
+        self.board = [' '] * 9
         self.current_player = "X"
 
-    def print_board(self):
-        print(f"[{self.board[0]}]-[{self.board[1]}]-[{self.board[2]}]")
-        print(f"[{self.board[3]}]-[{self.board[4]}]-[{self.board[5]}]")
-        print(f"[{self.board[6]}]-[{self.board[7]}]-[{self.board[8]}]\n")
-
-    def check_diagonal(self):
-        if self.board[0] == self.board[4] == self.board[8] != "":
-            return True
-        if self.board[2] == self.board[4] == self.board[6] != "":
-            return True
-        return False
-
-    def check_horizontal(self):
-        for i in [0, 3, 6]:
-            if self.board[i] == self.board[i+1] == self.board[i+2] != "":
-                return True
-        return False
-
-    def check_vertical(self):
-        for i in range(3):
-            if self.board[i] == self.board[i+3] == self.board[i+6] != "":
-                return True
-        return False
-
-    def check_board(self):
-        return self.check_diagonal() or self.check_horizontal() or self.check_vertical()
-
-    def manage_turn(self):
-        self.current_player = "O" if self.current_player == "X" else "X"
-
-    def update_board(self, position):
-        if self.board[position] == "":
-            self.board[position] = self.current_player
-            return True
-        else:
-            print("This position is already taken.\n")
-            return False
+    def display_board(self):
+        print("\n")
+        print(f" {self.board[0]} | {self.board[1]} | {self.board[2]}      1 | 2 | 3")
+        print("---+---+---    ---+---+---")
+        print(f" {self.board[3]} | {self.board[4]} | {self.board[5]}      4 | 5 | 6")
+        print("---+---+---    ---+---+---")
+        print(f" {self.board[6]} | {self.board[7]} | {self.board[8]}      7 | 8 | 9")
+        print("\n")
 
     def input_position(self):
         while True:
             try:
-                position = int(input(f"Player {self.current_player}, enter position (0-8): "))
-                if position < 0 or position > 8:
+                pos = int(input(f"Player {self.current_player}, choose a position (1-9): ")) - 1
+                if pos < 0 or pos > 8:
                     print("Invalid position. Try again.")
-                elif self.update_board(position):
+                elif self.board[pos] != ' ':
+                    print("Position already taken. Try again.")
+                else:
+                    self.board[pos] = self.current_player
                     break
             except ValueError:
-                print("Please enter a number between 0 and 8.")
+                print("Please enter a number between 1 and 9.")
+
+    def check_win(self):
+        b = self.board
+        wins = [
+            [0,1,2], [3,4,5], [6,7,8],  # rows
+            [0,3,6], [1,4,7], [2,5,8],  # columns
+            [0,4,8], [2,4,6]            # diagonals
+        ]
+        for line in wins:
+            if b[line[0]] == b[line[1]] == b[line[2]] != ' ':
+                return True
+        return False
 
     def is_board_full(self):
-        return all(cell != "" for cell in self.board)
+        return ' ' not in self.board
 
-    def start_game(self):
+    def switch_player(self):
+        self.current_player = "O" if self.current_player == "X" else "X"
+
+    def play(self):
         while True:
-            self.print_board()
+            self.display_board()
             self.input_position()
-
-            if self.check_board():
-                self.print_board()
-                print(f" Player {self.current_player} wins!")
+            if self.check_win():
+                self.display_board()
+                print(f"Player {self.current_player} wins!")
                 break
-
             if self.is_board_full():
-                self.print_board()
+                self.display_board()
                 print("It's a tie!")
                 break
+            self.switch_player()
 
-            self.manage_turn()
 
-
-# Start the game
-game = TicTacToe()
-game.start_game()
+# Run the game
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.play()
