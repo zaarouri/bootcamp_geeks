825cb9e5833d38daabf70b56cc4b23636384ba54
zaarouri <abdelmounime.zaarouri@gmail.com>
2025-07-01 16:42:44 +0100

    w2d1xp

---
+++ week2/day1/xp/exerciseXp.sql
@@ -0,0 +1,44 @@
+-- Create tables
+CREATE TABLE customers();  
+CREATE TABLE items();      
+
+ALTER TABLE items ADD COLUMN id SERIAL PRIMARY KEY;
+ALTER TABLE items ADD COLUMN name VARCHAR(50);
+ALTER TABLE items ADD COLUMN price DECIMAL(10,2);
+
+ALTER TABLE customers ADD COLUMN id SERIAL PRIMARY KEY;
+ALTER TABLE customers ADD COLUMN first_name VARCHAR(50);
+ALTER TABLE customers ADD COLUMN last_name VARCHAR(50);
+
+INSERT INTO items (name, price) VALUES ('small desk', 100);
+INSERT INTO items (name, price) VALUES ('large desk', 300);
+INSERT INTO items (name, price) VALUES ('fan', 80);
+
+INSERT INTO customers (first_name, last_name) VALUES ('Greg', 'Jones');
+INSERT INTO customers (first_name, last_name) VALUES ('Sandra', 'Jones');
+INSERT INTO customers (first_name, last_name) VALUES ('Scott', 'Scott');
+INSERT INTO customers (first_name, last_name) VALUES ('Trevor', 'Green');
+INSERT INTO customers (first_name, last_name) VALUES ('Melanie', 'Johnson');
+
+SELECT * FROM items;
+SELECT * FROM customers;
+
+
+
+SELECT * FROM items
+WHERE price >80;
+
+
+
+SELECT * FROM items
+WHERE price <300;
+
+SELECT * FROM customers
+WHERE last_name = 'Smith';
+
+SELECT * FROM customers
+WHERE last_name = 'Jones';
+
+
+SELECT * FROM customers
+WHERE last_name NOT LIKE 'Scott';
